694d44ec195ac468336ef965fbf61c208cbb2b2e
Perry Mitchell <perry@perrymitchell.net>
2018-08-16 23:05:48 +0300

    Add archive facade basics

---
+++ source/archive.js
@@ -0,0 +1,49 @@
+const { createEntryFacade } = require("./entry.js");
+
+function consumeArchiveFacade(archive, facade) {}
+
+function createArchiveFacade(archive) {
+    const entries = archive
+        .getGroups()
+        .reduce((output, group) => [...output, ...getEntriesFacades(group)], []);
+    return {
+        type: "archive",
+        id: archive.id,
+        groups: getGroupsFacades(archive, "0"),
+        entries
+    };
+}
+
+function createGroupFacade(group, parentID = "0") {
+    return {
+        id: group ? group.id : null,
+        title: group ? group.getTitle() : "",
+        parentID
+    };
+}
+
+function getEntriesFacades(entryCollection, groupID) {
+    const facades = entryCollection.getEntries().map(entry =>
+        Object.assign({}, createEntryFacade(entry), {
+            parentID: groupID
+        })
+    );
+    entryCollection.getGroups().forEach(group => {
+        facades.push(...getEntriesFacades(group, group.id));
+    });
+    return facades;
+}
+
+function getGroupsFacades(groupCollection, parentID) {
+    const facades = groupCollection.getGroups().map(group => createGroupFacade(group, parentID));
+    groupCollection.forEach(group => {
+        facades.push(...getGroupsFacades(group.getGroups(), group.id));
+    });
+    return facades;
+}
+
+module.exports = {
+    consumeArchiveFacade,
+    createArchiveFacade,
+    createGroupFacade
+};
--- source/index.js
+++ source/index.js
@@ -1,9 +1,13 @@
 const { consumeEntryFacade, createEntryFacade, getEntryFacadeType } = require("./entry.js");
 const { createFieldDescriptor } = require("./tools.js");
+const { consumeArchiveFacade, createArchiveFacade, createGroupFacade } = require("./archive.js");
 
 module.exports = {
+    consumeArchiveFacade,
     consumeEntryFacade,
+    createArchiveFacade,
     createEntryFacade,
+    createGroupFacade,
     createFieldDescriptor,
     getEntryFacadeType
 };
