c0d57955d417653313e12e6c33fdf9cf731ecfbd
Perry Mitchell <perry@perrymitchell.net>
2018-08-19 14:38:58 +0300

    Add group facade tests

--- test/unit/archive.spec.js
+++ test/unit/archive.spec.js
@@ -1,24 +1,27 @@
 const { Archive } = require("buttercup");
-const { createArchiveFacade } = require("../../source/archive.js");
+const { createArchiveFacade, createGroupFacade } = require("../../source/archive.js");
 
 describe("archive", function() {
-    describe("createArchiveFacade", function() {
-        beforeEach(function() {
-            this.archive = new Archive();
-            this.archive.setAttribute("ATTR_1", "one").setAttribute("ATTR_2", "two");
-            const topGroup = this.archive.createGroup("top");
-            const bottomGroup = topGroup.createGroup("one").createGroup("two");
-            topGroup.createGroup("three");
-            topGroup
-                .createEntry("Entry A")
-                .setProperty("username", "user@example.com")
-                .setProperty("password", "passw0rd");
-            bottomGroup
-                .createEntry("Entry B")
-                .setProperty("username", "user2@example.com")
-                .setProperty("password", "pa55w0rd");
-        });
+    beforeEach(function() {
+        this.archive = new Archive();
+        this.archive.setAttribute("ATTR_1", "one").setAttribute("ATTR_2", "two");
+        const topGroup = this.archive.createGroup("top");
+        const bottomGroup = topGroup.createGroup("one").createGroup("two");
+        topGroup.createGroup("three");
+        topGroup
+            .createEntry("Entry A")
+            .setProperty("username", "user@example.com")
+            .setProperty("password", "passw0rd");
+        bottomGroup
+            .createEntry("Entry B")
+            .setProperty("username", "user2@example.com")
+            .setProperty("password", "pa55w0rd");
+        this.group = topGroup;
+        topGroup.setAttribute("test_attr", "1234");
+        topGroup.setAttribute("test_attr_2", "5678");
+    });
 
+    describe("createArchiveFacade", function() {
         it("outputs the correct facade type", function() {
             const { type } = createArchiveFacade(this.archive);
             expect(type).to.equal("archive");
@@ -46,6 +49,33 @@ describe("archive", function() {
         it("outputs all entries", function() {
             const { entries } = createArchiveFacade(this.archive);
             expect(entries).to.have.lengthOf(2);
+        });
+    });
+
+    describe("createGroupFacade", function() {
+        it("outputs the ID", function() {
+            const facade = createGroupFacade(this.group);
+            expect(facade).to.have.property("id", this.group.id);
+        });
+
+        it("outputs the title", function() {
+            const facade = createGroupFacade(this.group);
+            expect(facade).to.have.property("title", this.group.getTitle());
+        });
+
+        it("sets the type", function() {
+            const facade = createGroupFacade(this.group);
+            expect(facade).to.have.property("type", "group");
+        });
+
+        it("outputs the group attributes", function() {
+            const facade = createGroupFacade(this.group);
+            expect(facade)
+                .to.have.property("attributes")
+                .that.deep.equals({
+                    test_attr: "1234",
+                    test_attr_2: "5678"
+                });
         });
     });
 });
