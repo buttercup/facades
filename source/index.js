a5020560d0e2ac772b3c8a373cc622dce096d48e
Perry Mitchell <perry@perrymitchell.net>
2020-01-08 21:31:46 +0200

    Expand+document symbols, add array of entry types

--- source/index.js
+++ source/index.js
@@ -6,38 +6,22 @@ const {
     createArchiveFacade,
     createGroupFacade
 } = require("./archive.js");
-const {
-    ENTRY_TYPE_CREDITCARD,
-    ENTRY_TYPE_LOGIN,
-    ENTRY_TYPE_NOTE,
-    ENTRY_TYPE_SSHKEY,
-    ENTRY_TYPE_WEBSITE,
-    FIELD_VALUE_TYPE_NOTE,
-    FIELD_VALUE_TYPE_OTP,
-    FIELD_VALUE_TYPE_PASSWORD,
-    FIELD_VALUE_TYPE_TEXT
-} = require("./symbols.js");
+const symbols = require("./symbols.js");
 
 /**
  * @module ButtercupFacades
  */
 
-module.exports = {
-    ENTRY_TYPE_CREDITCARD,
-    ENTRY_TYPE_LOGIN,
-    ENTRY_TYPE_NOTE,
-    ENTRY_TYPE_SSHKEY,
-    ENTRY_TYPE_WEBSITE,
-    FIELD_VALUE_TYPE_NOTE,
-    FIELD_VALUE_TYPE_OTP,
-    FIELD_VALUE_TYPE_PASSWORD,
-    FIELD_VALUE_TYPE_TEXT,
-    consumeArchiveFacade,
-    consumeGroupFacade,
-    consumeEntryFacade,
-    createArchiveFacade,
-    createEntryFacade,
-    createGroupFacade,
-    createFieldDescriptor,
-    getEntryFacadeType
-};
+module.exports = Object.assign(
+    {
+        consumeArchiveFacade,
+        consumeGroupFacade,
+        consumeEntryFacade,
+        createArchiveFacade,
+        createEntryFacade,
+        createGroupFacade,
+        createFieldDescriptor,
+        getEntryFacadeType
+    },
+    symbols
+);
--- source/symbols.js
+++ source/symbols.js
@@ -1,12 +1,89 @@
+/**
+ * Facade type flag for an entry (specifies what type of entry it is)
+ * @type {String}
+ * @memberof module:ButtercupFacades
+ */
+const ENTRY_FACADE_TYPE_ATTRIBUTE = "BC_ENTRY_FACADE_TYPE";
+
+/**
+ * Credit-card entry type
+ * @type {String}
+ * @memberof module:ButtercupFacades
+ */
+const ENTRY_TYPE_CREDITCARD = "credit_card";
+/**
+ * Login (default) entry type
+ * @type {String}
+ * @memberof module:ButtercupFacades
+ */
+const ENTRY_TYPE_LOGIN = "login";
+/**
+ * Note entry type
+ * @type {String}
+ * @memberof module:ButtercupFacades
+ */
+const ENTRY_TYPE_NOTE = "note";
+/**
+ * SSH public/private key-pair entry type
+ * @type {String}
+ * @memberof module:ButtercupFacades
+ */
+const ENTRY_TYPE_SSHKEY = "ssh_key";
+/**
+ * Website entry type (includes URL)
+ * @type {String}
+ * @memberof module:ButtercupFacades
+ */
+const ENTRY_TYPE_WEBSITE = "website";
+
+/**
+ * Entry types collection (all available)
+ * @type {Array.<String>}
+ * @memberof module:ButtercupFacades
+ */
+const ENTRY_TYPES = [
+    ENTRY_TYPE_CREDITCARD,
+    ENTRY_TYPE_LOGIN,
+    ENTRY_TYPE_NOTE,
+    ENTRY_TYPE_SSHKEY,
+    ENTRY_TYPE_WEBSITE
+];
+
+/**
+ * Note type entry field value
+ * @type {String}
+ * @memberof module:ButtercupFacades
+ */
+const FIELD_VALUE_TYPE_NOTE = "note";
+/**
+ * OTP (One Time Password) type entry field value
+ * @type {String}
+ * @memberof module:ButtercupFacades
+ */
+const FIELD_VALUE_TYPE_OTP = "otp";
+/**
+ * Password type entry field value
+ * @type {String}
+ * @memberof module:ButtercupFacades
+ */
+const FIELD_VALUE_TYPE_PASSWORD = "password";
+/**
+ * Text (default) type entry field value
+ * @type {String}
+ * @memberof module:ButtercupFacades
+ */
+const FIELD_VALUE_TYPE_TEXT = "text";
+
 module.exports = {
-    ENTRY_FACADE_TYPE_ATTRIBUTE: "BC_ENTRY_FACADE_TYPE",
-    ENTRY_TYPE_CREDITCARD: "credit_card",
-    ENTRY_TYPE_LOGIN: "login",
-    ENTRY_TYPE_NOTE: "note",
-    ENTRY_TYPE_SSHKEY: "ssh_key",
-    ENTRY_TYPE_WEBSITE: "website",
-    FIELD_VALUE_TYPE_NOTE: "note",
-    FIELD_VALUE_TYPE_OTP: "otp",
-    FIELD_VALUE_TYPE_PASSWORD: "password",
-    FIELD_VALUE_TYPE_TEXT: "text"
+    ENTRY_FACADE_TYPE_ATTRIBUTE,
+    ENTRY_TYPE_CREDITCARD,
+    ENTRY_TYPE_LOGIN,
+    ENTRY_TYPE_NOTE,
+    ENTRY_TYPE_SSHKEY,
+    ENTRY_TYPE_WEBSITE,
+    ENTRY_TYPES,
+    FIELD_VALUE_TYPE_NOTE,
+    FIELD_VALUE_TYPE_OTP,
+    FIELD_VALUE_TYPE_PASSWORD,
+    FIELD_VALUE_TYPE_TEXT
 };
